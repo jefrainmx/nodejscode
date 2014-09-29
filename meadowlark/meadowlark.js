65a9eabac18aeca183ab197556b753c52f45c545
efrain gtz <jefrainmx@gmail.com>
2014-09-29 11:03:50 -0500

    update

--- meadowlark/meadowlark.js
+++ meadowlark/meadowlark.js
@@ -7,12 +7,23 @@ var app = express();
 var handlebars = require('express3-handlebars').create({ defaultLayout:'main' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + '/public'));
+
+var fortunes = [
+"Conquer your fears or they will conquer you.",
+"Rivers need springs.",
+"Do not fear what you don't know.",
+"You will have a pleasant surprise.",
+"Whenever possible, keep it simple.",
+];	
 
 app.get('/', function(req, res) {
 	res.render('home');
 });
 app.get('/about', function(req, res) {
-	res.render('about');
+	var randomFortune =
+	fortunes[Math.floor(Math.random() * fortunes.length)];
+	res.render('about', { fortune: randomFortune });
 });
 // 404 catch-all handler (middleware)
 app.use(function(req, res, next){
